0503f37550fcf6fe88fabfedc1413d0f0c71cac4
sudeep162002 <80563363+sudeep162002@users.noreply.github.com>
2021-05-04 13:34:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+function updatemap() {
+    console.log("updating data")
+  fetch("/data.json")
+    .then((response) => response.json())
+    .then((rsp) => {
+      console.log(rsp.data);
+      rsp.data.forEach((element) => {
+        latitude = element.latitude;
+        longitude = element.longitude;
+        cases=element.infected;
+        if(cases>255){
+            color="rgb(255,0,0)";
+        }
+        else{
+            color=`rgb(${cases},0,0)`;
+
+        }
+
+        var marker = new mapboxgl.Marker({
+          draggable: false,
+         color: color 
+        })
+          .setLngLat([longitude, latitude])
+          .addTo(map);
+      });
+    });
+}
+let interval=20000;
+setInterval(updatemap,interval);
+updatemap();
